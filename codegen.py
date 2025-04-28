d4ae22e0f1affc4941a4b482414dd2261f4e4d99
Richard <richard@stromer.org>
2025-04-28 17:11:17 +0200

    extract codegen into dedicated file

---
+++ codegen.py
@@ -0,0 +1,33 @@
+import json
+import subprocess
+from pathlib import Path
+
+from fastapi import FastAPI
+from fastapi.concurrency import asynccontextmanager
+
+
+def generate_schema(app: FastAPI):
+    schema_definition = app.openapi()
+
+    schema_path = Path(".schema")
+    schema_path.mkdir(exist_ok=True)
+
+    with open(schema_path / "openapi.json", "w") as f:
+        json.dump(schema_definition, f, indent=2)
+
+    subprocess.run(
+        [
+            "bun",
+            "run",
+            "openapi-typescript",
+            schema_path / "openapi.json",
+            "-o",
+            schema_path / "openapi.d.ts",
+        ]
+    )
+
+
+@asynccontextmanager
+async def lifespan(app: FastAPI):
+    generate_schema(app)
+    yield
