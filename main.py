2a717aca3448fe24fca33d263ca660112240aa69
Richard <richard@stromer.org>
2025-04-28 17:07:54 +0200

    auto generate schema definitions with fastapi

--- main.py
+++ main.py
@@ -1,9 +1,39 @@
 import asyncio
+
 from datetime import datetime
+import json
+from pathlib import Path
+import subprocess
 
 from fastapi import FastAPI
+from fastapi.concurrency import asynccontextmanager
 
-app = FastAPI()
+
+@asynccontextmanager
+async def lifespan(app: FastAPI):
+    """Lifespan context manager for FastAPI app"""
+    schema_definition = app.openapi()
+
+    schema_path = Path(".schema")
+    schema_path.mkdir(exist_ok=True)
+
+    with open(schema_path / "openapi.json", "w") as f:
+        json.dump(schema_definition, f, indent=2)
+
+    subprocess.run(
+        [
+            "bun",
+            "run",
+            "openapi-typescript",
+            schema_path / "openapi.json",
+            "-o",
+            schema_path / "openapi.d.ts",
+        ]
+    )
+    yield
+
+
+app = FastAPI(lifespan=lifespan)
 
 
 @app.get("/")
